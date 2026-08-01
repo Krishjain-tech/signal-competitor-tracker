67a26666a6a6a4ca50d8682d900e9b58afae3ca2
Krishjain-tech <kjainn330k@gmail.com>
2026-08-01 12:38:26 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+git clone https://github.com/Krishjain-tech/signal-competitor-tracker.git
+cd signal-competitor-tracker
+# copy app.py, requirements.txt, secrets.toml.example into this folder
+git add .
+git commit -m "Add app code"
+git push
